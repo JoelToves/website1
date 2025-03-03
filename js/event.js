13b8179b317c00279b5ddc69336b7c7988f66cea
JoelToves <joeltoves0@gmail.com>
2025-03-03 18:21:11 +0000

    Mon, Mar 3, 2025, 10:21 AM -08:00

--- js/event.js
+++ js/event.js
@@ -1,5 +1,10 @@
 const element1 = document.getELementById('element1')
 
 element1.addEventListener('click', ()=>{
-    element1.innerHTML = "
+    element1.innerHTML = "I've been clicked! <break> Try double clicking me."
+})
+
+element1.addEventListener('dblclick', ()=>{
+    element1.innerHTML = "I've been double clicked! <br>
+    Try single clickingme."
 })
