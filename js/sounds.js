55a7609db0289f5ca6274a01e361fa3dc7ca3d41
JoelToves <joeltoves0@gmail.com>
2025-03-12 15:44:50 +0000

    Wed, Mar 12, 2025, 8:44 AM -07:00

---
+++ js/sounds.js
@@ -0,0 +1,24 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+
+sounds.forEach(sound => {
+    const btn =  document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerHTML = sound
+    btn.addEveentListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+
+
+
+    document.getElementById('buttons').appendChild(btn)
+
+})
+function stopSongs(){
+    sounds.forEach(sound => {
+        document.getElementById(sound).pause()
+        document.getElementbyId(sound).currentTime = 0;
+    })
+}
