238b5c4a62f35b860a798d4428031b4757d88af5
JoelToves <joeltoves0@gmail.com>
2025-03-14 16:55:54 +0000

    Fri, Mar 14, 2025, 9:55 AM -07:00

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,10 @@
+const nav = document.querySelector('.nav')
 
+window.addEventlistener('scroll', () => {
+    if (window,scrollY > 160){
+        nav.classList.add('active')
+    }
+    else {
+        nav.classList.remove('active')
+    }
+})
