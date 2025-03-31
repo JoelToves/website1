7220da20fb41a946dac1c860682c06255199ac2c
JoelToves <joeltoves0@gmail.com>
2025-03-31 16:59:12 +0000

    Mon, Mar 31, 2025, 9:59 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -3,6 +3,16 @@ const text = document.getElementById('text')
 
 
 function breathAnimation() {
-    text.innerHTML = 'Breath In'
-    container.classList.add(grow)
+    text.innerHTML = 'Breathe In'
+    container.classList.add('grow')
+
+    setTimeout(() => {
+        text.innerHTML = 'Hold'
+    }, 3000)
+
+    setTImeout( () => {
+        text.innerHTML = 'Breathe Out!'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 4500)
 }
