f65a9cb365f67e5385e47d743163dcfde724d9de
JoelToves <joeltoves0@gmail.com>
2025-03-31 16:54:54 +0000

    Mon, Mar 31, 2025, 9:54 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -1,2 +1,8 @@
 const container = document.getElementById('container')
 const text = document.getElementById('text')
+
+
+function breathAnimation() {
+    text.innerHTML = 'Breath In'
+    container.classList.add(grow)
+}
